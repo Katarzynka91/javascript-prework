e30a9d65409dbee25cb5254ddea8b78e7b76edf0
kasia.rogowska11@gmail.com <kasia.rogowska11@gmail.com>
2023-05-19 13:37:03 +0200

    uruchomienie funkcji 2

--- js/script.js
+++ js/script.js
@@ -65,9 +65,9 @@ function displayResult(argComputerMove, argPlayerMove){
   
 }
 
-function displayResult (computerMove,PlayerMove)
+function displayResult (computerMove,playerMove)
 
-console.log(1,2)
+
 
 
 
